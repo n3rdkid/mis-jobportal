bd2a7cf1119b5bc0d55e0b4e90e3b2355024b5e5
Saurav Adhikari <sauravads123@gmail.com>
2018-12-19 14:28:23 +0545

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+gulp.task('sass',function(){
+	return gulp.src('app/scss/**/*.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('app/css'))
+})
+gulp.task('watch', function(){
+    gulp.watch('app/scss/main.scss', gulp.series(sass));
+});
